43c134fa67ea94ef1f8a57f30ae67396294db337
sushadas <sushadas@deloitte.com>
2023-11-10 18:00:35 +0000

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  backend "gcs" {
+    bucket  = "roidtc-projects-222-tf-state"
+    prefix  = "terraform/state"
+  }
+
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.41.0"
+    }
+  }
+}
+
+
+provider "google" {
+
+  project = var.project_id
+
+}
